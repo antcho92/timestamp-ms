b97fd782504485e87185ec8b212cd1f6bc8363b2
Anthony <antcho92@gmail.com>
2016-08-17 23:05:06 +0000

    Updated html

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ var moment = require('moment');
 var path = require('path');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 //GET home page
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  var fileName = path.join(__dirname, 'index.html');
+  res.sendFile(fileName, function(err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+  });
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.get('/:timestamp', function(req, res) {
+  var time = moment(req.params.timestamp);
+  res.json({
+    'natural': time.format('MM DD, YYYY'),
+    'unix': time.format('X')
+  });
+});
+
+app.listen(port, function () {
+  console.log('Server listening on port ' + port);
 });
